c7bf9b6b024582a8e487856f1e1bd1758475bc28
shubham-2094 <shubham2094@yahoo.com>
2023-04-11 23:27:29 +0530

    array and object destructuring

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fetch = require("node-fetch");
 //////////////////////  Map Method //////////////////////
 
 const people = [
@@ -210,7 +211,7 @@ const total = cart.reduce(
     cartTotal: 0,
   }
 );
-console.log("total", total);
+// console.log("total", total);
 
 // Other method
 let { totalItem, cartTotal } = cart.reduce(
@@ -226,4 +227,87 @@ let { totalItem, cartTotal } = cart.reduce(
   }
 );
 cartTotal = parseFloat(cartTotal.toFixed(2));
-console.log("other method", totalItem, cartTotal);
+// console.log("other method", totalItem, cartTotal);
+
+const url = "https://api.github.com/users/john-smilga/repos?per_page=100";
+
+const fetchRepo = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  const newData = data.reduce((total, repo) => {
+    const { language } = repo;
+    if (language) {
+      total[language] = total[language] + 1 || 1;
+    }
+    return total;
+  }, {});
+  // console.log("new data", newData);
+};
+fetchRepo();
+
+////////////////////////// Array Destructuring ///////////////////////
+// faster or easier way to unpack/access the variable from arrays
+
+const fruits = ["orange", "banana", "apple", "grapes"];
+
+const friends = ["Kiran", "Mandar", "Rahul", "Pushpak"];
+
+const fruit1 = fruits[0];
+const fruit2 = fruits[1];
+const fruit3 = fruits[2];
+
+console.log("fruits", fruit1, fruit2, fruit3);
+
+// let [Kiran, Mandar, Rahul, Pushapk] = friends;
+
+// if you add any new variable in array it gives undefined
+//david not in the list david is undefined
+// variable name can be anything
+const [, asasdds, , Pushapk, David] = friends;
+
+console.log("Friends", asasdds, Pushapk);
+
+let first = "Shubham";
+
+let second = "Patil";
+
+// variable swapping
+// let temp = second;
+// second = first;
+// first = temp;
+
+// using Destructuring
+
+[second, first] = [first, second];
+
+console.log("name", first, second);
+
+////////////////////// Object Destructuring ///////////////////////
+//get undefined if key is not in the object
+//order matter in the arrays but object dosen't matter order
+const bob = {
+  firstName: "bob",
+  lastName: "willey",
+  city: "new jersy",
+  sibllings: {
+    sister: "jane",
+  },
+};
+let {
+  lastName,
+  firstName,
+  city,
+  sibllings: { sister: sister },
+  zip,
+} = bob;
+console.log("object", firstName, lastName, city, sister, zip);
+
+function printPerson(person) {
+  const {
+    firstName,
+    lastName,
+    sibllings: { sister: sister },
+  } = person;
+  console.log("full name", firstName, lastName, sister);
+}
+printPerson(bob);
